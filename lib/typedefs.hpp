b8fc5de0188aad41429bc8f323fcd3200736dd1e
a-berg <adberenf@gmail.com>
2019-05-01 12:13:10 +0200

    chore: I don't remember last status
    I don't know what changed from last commit, but basically I am making this one
    as an "anchor point" to start creating new branches for refactoring and other development.
    
    Oh, I added a readme, but it's gitignored.

--- lib/typedefs.hpp
+++
@@ -1,7 +0,0 @@
-#if !defined(TYPE_DEFINITIONS)
-#define TYPE_DEFINITIONS
-
-typedef unsigned long int u32;
-typedef unsigned long long int u64;
-
-#endif // TYPE_DEFINITIONS
--- src/miller_rabin.hpp
+++ src/miller_rabin.hpp
@@ -1,6 +1,5 @@
-#include "../lib/numth.hpp"
-
-
+#include <basic_operations.hpp>
+#include <random>
 
 bool miller_rabin_test(const uint64_t &number, const uint64_t &acc)
 {
@@ -47,57 +46,3 @@ bool miller_rabin_test(const uint64_t &number, const uint64_t &acc)
     }
     return true; // all tests passed, good chance that n is prime
 };
-
-uint64_t powmod(uint64_t base, uint64_t exponent, uint64_t modulus)
-{
-    uint64_t r = modulus==1?0:1;
-    while (exponent > 0) {
-        if(exponent & 1)
-            r = mulmod(r, base, modulus);
-        exponent >>= 1;
-        base = mulmod(base, base, modulus);
-    }
-    return r;
-}
-
-uint64_t mulmod(uint64_t a, uint64_t b, uint64_t m) {
-    uint64_t res = 0;
-    uint64_t temp_b;
-
-    b %= m;
-
-    while (a != 0) {
-        if (a & 1) {
-            /* Add b to res, modulo m, without overflow */
-            if (b >= m - res) /* Equiv to if (res + b >= m), without overflow */
-                res -= m;
-            res += b;
-        }
-        a >>= 1;
-
-        /* Double b, modulo m */
-        temp_b = b;
-        if (b >= m - b)       /* Equiv to if (2 * b >= m), without overflow */
-            temp_b -= m;
-        b += temp_b;
-    }
-    return res;
-}
-
-// Finds the biggest prime p such that p <= number
-uint64_t prime_leq(uint64_t number) {
-
-    while(number != 0 && !miller_rabin_test(number, 128)) {
-        number--;
-    }
-    return number;
-}
-
-// Finds all the sexy prime pairs (p,p+6) such that p+6 <= number
-uint64_t sexy_primes(uint64_t number) {
-
-    while(number != 0 && !miller_rabin_test(number, 128)) {
-        number--;
-    }
-    return number;
-}
